90539ae9a1ce468406dbc5bf6d489baa254b4b89
Bala-200 <bkaliyamurthy@deloitte.com>
2019-04-30 13:06:14 +0000

    Changes made

--- deploy.cmd
+++ deploy.cmd
@@ -104,9 +104,10 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
 IF EXIST "%DEPLOYMENT_SOURCE%/angular.json" (
   echo Building App in %DEPLOYMENT_SOURCE%…
   pushd "%DEPLOYMENT_SOURCE%"
-  :: call :ExecuteCmd !NPM_CMD! run build
+  echo Entering into the folder
+  call :ExecuteCmd !NPM_CMD! run build
   :: If the above command fails comment above and uncomment below one
-  call ./node_modules/.bin/ng build –prod
+  ::call ./node_modules/.bin/ng build –prod
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
