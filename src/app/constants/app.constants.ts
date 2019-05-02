691f2054397926da3d306c01eb61be82a9268b8f
Bala-200 <bkaliyamurthy@deloitte.com>
2019-05-02 08:43:14 +0000

    URL changes

--- src/app/constants/app.constants.ts
+++ src/app/constants/app.constants.ts
@@ -1,3 +1,3 @@
-export const URL = 'http://127.0.0.1';
+export const URL = 'https://argusapi.azurewebsites.net';
 export const PORT = '6000';
-export const BASEURL = 'http://localhost:5602/api/upload'
+export const BASEURL = 'https://argusapi.azurewebsites.net/api/upload'
